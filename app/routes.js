c383844d0704c5e680719fa1ca4c338742fcd9d2
Peter Yates <peter.yates@graphia.co.uk>
2022-09-18 13:47:18 +0100

    Add 'how many objects' registration page

--- app/routes.js
+++ app/routes.js
@@ -17,4 +17,24 @@ router.post('/register/which-objects', function callback(request, response) {
   response.redirect('/register/how-many-objects');
 });
 
+router.get('/register/how-many-objects', function callback(request, response) {
+  console.debug("on how many page")
+
+  console.debug("session data:", request.session.data);
+
+  // here we're passing a second argument to 'render', the keys will be available
+  // as variables within the template
+  response.render('register/how-many-objects', {
+    question: `How many ${request.session.data["which-object"]} can you juggle at once?` }
+  );
+});
+
+router.post('/register/how-many-objects', function callback(request, response) {
+  console.debug("submitting which objects page")
+
+  console.debug("session data:", request.session.data);
+
+  response.redirect('/register/complete');
+});
+
 module.exports = router
